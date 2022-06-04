18967881e896211c27228f6545774480ed67a113
ZuandaBaransyahPutra <zuandabaransyahputra@gmail.com>
2022-06-04 17:00:40 +0700

    add costum hooks useScroll dan useModal

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,17 @@
+import React from 'react';
 import Clients from 'parts/Clients';
 import Footer from 'parts/Footer';
 import Header from 'parts/Header';
-import Hero from 'parts/Hero';
+import Hero from 'parts/HomePage/Hero';
 import BrowseRoom from 'parts/HomePage/BrowseRoom';
 import JustArrived from 'parts/HomePage/JustArrived';
 import SiteMap from 'parts/SiteMap';
-import React from 'react';
+import useScrollAnchor from 'helpers/hooks/useScrollAnchor';
+import useModalDOM from 'helpers/hooks/useModalDOM';
 
 const HomePage = () => {
+  useScrollAnchor();
+  useModalDOM()
   return (
     <>
       <Header theme={"white"} position="absolute" />
